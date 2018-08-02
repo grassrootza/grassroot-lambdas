41df27f7b92845947e374692354a6d761043e502
Luke Jordan <luke.jordan@grassroot.org.za>
2018-08-02 21:16:01 +0200

    Extended, abstracted, also added final logging

--- whatsAppController/app.js
+++ whatsAppController/app.js
@@ -12,18 +12,65 @@ AWS.config.update({
 });
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.json());
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 app.post('/inbound', (req, res) => {
-    console.log('Got a message! Query params: ', req.query);
-    console.log('And body? ', req.body);
-    const twiml = new MessagingResponse();
+    const content = getMessageContent(req);
+    console.log('decoded content: ', content); 
 
-    twiml.message('The Robots are coming! Head for the hills!');
-  
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+    const response = getMessageReply(content);
+    res.writeHead(200, {'Content-Type': response.content_type});
+    res.end(response.body);
+
+    logIncoming(content);
 });
 
-module.exports = app;
+// module.exports = app;
+app.listen(3000, () => console.log(`Listening on port 3000`));
+
+// we will be swapping these out in future, as possibly / probably not using Twilio, so stashing them
+
+const getMessageContent = (req) => {
+    console.log('message body: ', req.body);
+    const incoming_text = req.body['Body'];
+    const incoming_phone = req.body['From'] ? req.body['From'].substring('whatsapp:+'.length) : '<<Error>>';
+    return {
+        'message': incoming_text,
+        'from': incoming_phone
+    };
+}
+
+const getMessageReply = (content) => {
+    const twiml = new MessagingResponse();
+
+    // note: !! this appears to be FILO, hence do in reverse order
+    twiml.message('Also, here is what we think you said: ' + content.message);
+
+    twiml.message('Oh, the things Grassroot will do with WhatsApp. ' + 
+        'What they are, yet we know not; but they shall be the terrors of the earth.');
+
+    return {
+        content_type: 'text/xml',
+        body: twiml.toString() 
+    }
+}
+
+const logIncoming = (content) => {
+    var params = {
+        TableName: 'chatConversationLogs',
+        Item: {
+          'userId': content.from,
+          'timestamp': Date.now(),
+          'message': content.message
+        }
+    };
+
+    docClient.put(params, (err, data) => {
+        if (err)
+            console.log('Aargh, error: ', JSON.stringify(err, null, 2));
+        else
+            console.log('Worked! Result: ', JSON.stringify(data, null, 2));
+    });
+}
