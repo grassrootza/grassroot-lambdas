83fdb9aaf9981198f75cd2b2b68b9eed244c5eed
Luke Jordan <luke.jordan@grassroot.org.za>
2018-08-06 21:12:35 +0200

    Further build out, now have some sequential handling

--- whatsAppController/app.js
+++ whatsAppController/app.js
@@ -12,9 +12,10 @@ const AWS = require('aws-sdk');
 const conversation = require('./conversation-en.json'); // in time maybe switch to reading this from S3 ...?
 
 const CUTOFF_CONVERSATION_LENGTH = 1; // in hours; in production, will adjust to 3, leaving 1 here for easier testing
+const START_WORD = /start/i; // in case conversation 'finished', but want to re-initiate
 
 AWS.config.update({
-    region: "eu-west-1",
+    region: 'eu-west-1',
 });
 
 const app = express();
@@ -35,7 +36,7 @@ app.post('/inbound', async (req, res, next) => {
         console.timeEnd('get_prior');
 
         console.time('assemble_response');
-        const response = await getMessageReply(content, lastMessage['Items'][0]);
+        const response = await getMessageReply(content, lastMessage ? lastMessage['Items'][0] : null);
         console.timeEnd('assemble_response');
 
         if (response) {
@@ -48,10 +49,16 @@ app.post('/inbound', async (req, res, next) => {
             res.writeHead(200, {'Content-Type': RESPONSE_CONTENT_TYPE});
             res.end(response.body);
         } else {
-            res.status(200).end();
+            res.writeHead(200, {'Content-Type': RESPONSE_CONTENT_TYPE});
+            res.end(emptyMsgBody());
         }
     } catch (e) {
-        next(e);
+        console.log('Error: ', e); // todo: stick in a DLQ, or report some other way
+        console.log('Gracefully exiting ...');
+        const response = await assembleErrorMsg('server');
+        res.writeHead(200, {'Content-Type': RESPONSE_CONTENT_TYPE});
+        console.log('should return: ', response);
+        res.end(response.body);
     }
 
 });
@@ -90,6 +97,12 @@ interpretMessage = (message_text) => {
 // if (3), then need to know if brand new user, or returning user, and hence whether to include a greeting
 
 const getMessageReply = async (content, prior) => {
+    // console.time('nlu_call');
+    // const thisMsgInterpred = await interpretMessage(content.message);
+    // console.log('message interpreted: ', thisMsgInterpred);
+    // console.timeEnd('nlu_call');
+    // console.log('interpreted msg: ', thisMsgInterpred);
+    
     if (prior) {
         console.log('Have prior message: ', prior);
         return continueConversation(content, prior);
@@ -97,24 +110,19 @@ const getMessageReply = async (content, prior) => {
         return handleNewConversation(content);
     }
     
-    // console.time('nlu_call');
-    // const thisMsgInterpred = await interpretMessage(content.message);
-    // // console.log('message interepreted: ', thisMsgInterpred);
-    // console.timeEnd('nlu_call');
-
-    // console.log('interpreted msg: ', thisMsgInterpred);
+    
 }
 
 const handleNewConversation = async (content) => {
     // note: message ordering seems pretty unpredictable here
     const block = getRelevantConversationBlock('opening');
-    const body = getResponseChunk(block, "1", 0);
+    const body = getResponseChunk(block, 'start', 0);
 
     const messages = extractMessages(block, body);
 
     let reply = {
         body: turnMsgsIntoBody(messages.replyMsgs),
-        context: 'open:::id-1',
+        context: 'open:::id-start',
         replies: messages.replyMsgs
     };
 
@@ -125,12 +133,62 @@ const handleNewConversation = async (content) => {
 }
 
 const continueConversation = async (content, prior) => {
-    console.log('prior: ', prior);
-    if (prior['context'] == 'error::id-unimplemented') {
+    // console.log('continuing conversation, prior chat: ', prior);
+
+    if (content.message.match(START_WORD))
+        return handleNewConversation(content);
+
+    if (prior['context'] && prior['context'].startsWith('error')) {
         console.log('we have ended the conversation, stop doing anything more');
     } else {
-        return assembleErrorMsg('unimplemented');
+        let optionTriggered = traceOptions(content.message, prior);
+        console.log('found option: ', optionTriggered);
+        const nextMenu = optionTriggered ? assembleLaterMsg(optionTriggered) : null;
+        return nextMenu ? nextMenu : assembleErrorMsg('unimplemented');
     }
+}
+
+const traceOptions = (userMessage, priorSent) => {
+    const optionKeys = priorSent['options'];
+    console.log('prior options: ', optionKeys);
+    const digitMatch = /\d+[\W+]?$/
+    if (userMessage.match(digitMatch)) {
+        console.log('User sent a digit! Extract it and return option key');
+        const digit = parseInt(userMessage.replace( /^\D+/g, '')) - 1;
+        return optionKeys[digit];
+    } else {
+        const foundOption = optionKeys.find(option => regexCheckForOption(option, userMessage));
+        console.log('found option? : ', foundOption);
+        return foundOption;
+    }
+}
+
+const regexCheckForOption = (option, userMessage) => {
+    const regex = new RegExp('(.+)?(' + option + ')(.+)?', 'i');
+    return userMessage.match(regex);
+}
+
+const assembleLaterMsg = (optionKey) => {
+    const block = getRelevantConversationBlock(optionKey);
+    if (!block)
+        return assembleErrorMsg('unimplemented');
+
+    const body = getResponseChunk(block, 'start', 0);
+    if (!body)
+        return assembleErrorMsg('unimplemented');
+
+    const messages = extractMessages(block, body);
+
+    let reply = {
+        body: turnMsgsIntoBody(messages.replyMsgs),
+        context: 'open:::id-1', // next, need to figure this out
+        replies: messages.replyMsgs
+    };
+
+    if (messages.optionsKeys)
+        reply.options = messages.optionsKeys;
+    
+    return reply;
 }
 
 const assembleErrorMsg = async (msgId) => {
@@ -141,7 +199,7 @@ const assembleErrorMsg = async (msgId) => {
 
     return {
         body: turnMsgsIntoBody(messages.replyMsgs),
-        context: 'error::id-unimplemented',
+        context: 'error::id-' + msgId,
         replies: messages.replyMsgs
     }
 }
@@ -152,7 +210,7 @@ const getRelevantConversationBlock = (section) => {
 }
 
 const getEntity = (block, id) => {
-    console.log('looking for ' + id + ' in block: ', block);
+    // console.log('looking for ' + id + ' in block: ', block);
     return block.find(item => item._id == id);
 }
 
@@ -202,9 +260,14 @@ const turnMsgsIntoBody = (replyMsgs) => {
     return twiml.toString();
 }
 
+const emptyMsgBody = () => {
+    const twiml = new MessagingResponse();
+    return twiml.toString();
+}
+
 const getMostRecent = (content) => {
     const userMsisdn = content.from;
-    const cutoff = hoursInPast(3);
+    const cutoff = hoursInPast(CUTOFF_CONVERSATION_LENGTH);
     console.log('timestamp in past: ', cutoff);
     const params = {
         'TableName': 'chatConversationLogs',
