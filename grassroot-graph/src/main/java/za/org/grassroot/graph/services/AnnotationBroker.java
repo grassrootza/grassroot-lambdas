092cb0c7391b92630e1df1ef5ae76870a69a40f9
luke-grassroot <luke.jordan@grassroot.org.za>
2018-08-16 09:16:20 +0200

    Added missing persistence line in lambda

--- grassroot-graph/src/main/java/za/org/grassroot/graph/services/AnnotationBroker.java
+++ grassroot-graph/src/main/java/za/org/grassroot/graph/services/AnnotationBroker.java
@@ -1,8 +1,5 @@
 package za.org.grassroot.graph.services;
 
-import za.org.grassroot.graph.domain.enums.GrassrootRelationship;
-
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
